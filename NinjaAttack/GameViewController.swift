4cca673cb3b3631bdd70216dbc256fbb679a8140
Lars Larsson <lars@dreamly.se>
2019-04-09 09:23:28 +0900

    Initial commit

---
+++ NinjaAttack/GameViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    let scene = GameScene(size: view.bounds.size)
+    let skView = view as! SKView
+    skView.showsFPS = true
+    skView.showsNodeCount = true
+    skView.ignoresSiblingOrder = true
+    scene.scaleMode = .fill
+    skView.presentScene(scene)
+  }
+  
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+}
